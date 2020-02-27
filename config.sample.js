af0b99bdccbd9da926736a763c128335f74a5db6
octav <octav@behigh.es>
2020-02-27 18:42:49 +0200

    Added config sample

---
+++ config.sample.js
@@ -0,0 +1,14 @@
+const config = {
+	host: 'localhost', // Database host
+	port: 3306, // Database port
+	user: 'root', // Database user
+	password: '', // Database password
+	database: '', // Database name
+	tables: [], // List of tables to backup
+	ignoreTables: [], // List of tables to ignore
+	target_path: './backups', // Backup path, should by exists
+	fileName: '[database].sql'
+
+}
+
+module.exports = config
